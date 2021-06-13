159c856ef7689b4ac9d3df85e8df570e837b7504
Faiz Ahmadhio Herman <faizadhio@gmail.com>
2021-06-13 15:15:55 +0700

    Initial Commit

---
+++ MovieTMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTMDB
+//
+//  Created by Faiz Ahmadhio Herman on 13/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
